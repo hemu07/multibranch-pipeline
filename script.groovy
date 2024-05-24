e0e21ab4dbf8a35e0961f03764b79238950a8e43
hemu07 <90203539+hemu07@users.noreply.github.com>
2024-05-24 22:52:48 +0530

    Update script.groovy
    added test stage

--- script.groovy
+++ script.groovy
@@ -1,19 +1,27 @@
-def buildJar() {
-    echo "building the application..."
+def testApp() {
+    echo "testing the app"
+    echo "executing pipeline for branch ${BRANCH_NAME}"
+}
+
+def buildJarFile() {
+    echo "Building the application..."  
     sh 'mvn package'
-} 
+}
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    echo "building the dockerimage.."
+                    withCredentials([ 
+                        usernamePassword(credentialsId: 'docker-hub-creds', usernameVariable: 'USER', passwordVariable: 'PAT')
+                    ]) { 
+                        sh "docker build -t hemu07/hemali_repo:jma-2.0 . "
+                        sh "echo ${PAT} | docker login -u ${USER} --password-stdin"
+                        sh "docker push hemu07/hemali_repo:jma-2.0"
     }
-} 
+}
+
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo "Deploying the application..."
+}
 
 return this
